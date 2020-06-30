a8aa919e888799219d5888290d98ebc2bbf5afb7
Lucas Sanders <lucasbsanders@gmail.com>
2020-06-30 00:52:52 -0700

    Revert "Kivy code updates"
    This reverts commit c62d771a13d9d79706f15313411d79ae1d9b546e.

--- app_home/app/__init__.py
+++ app_home/app/__init__.py
@@ -1,9 +1,6 @@
-import kivy
-kivy.require('2.0.0rc3s')
-
 from kivy.app import App
 from .view import MainWindow
 
-class Poppy(App):
+class MainApp(App):
     def build(self):
         return MainWindow()
